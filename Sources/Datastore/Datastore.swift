deaa4d489c0d6c0d7fa2134b5004f08fbc731027
Mathew Perkins <mathewperkins@Mathews-MacBook-Pro.local>
2025-04-02 20:08:10 +1100

    feat: init

---
+++ Sources/Datastore/Datastore.swift
@@ -0,0 +1,167 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import Combine
+
+enum Status {
+    case OK
+    case ERROR
+}
+
+struct DatastoreError: Error {
+    let message: String
+}
+
+struct Result<T: Codable> {
+    var status : Status
+    var err : DatastoreError?
+    var obj: T?
+}
+
+struct Directory {
+    var name : String
+    
+    func GetPath() -> String {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docDir = paths[0]
+        return docDir.appendingPathComponent(self.name).path
+    }
+}
+
+class DataStore {
+    
+    static func GetDirectory(_ name : String) -> Directory {
+        return Directory(name: name)
+    }
+    
+    static func SaveGeneric<T: Encodable>(_ dir : Directory, fname : String, data: T) -> Result<T> {
+        if let err = writeToDisk(dir: dir.GetPath(), fName: fname, data: data) {
+            return Result(status: .ERROR, err: err, obj: nil)
+        }
+        return Result(status: .OK, err: nil, obj: data)
+    }
+    
+    static func LoadGeneric<T: Decodable>(_ dir: Directory, fname: String, as type: T.Type) -> Result<T> {
+        return readFromDisk(dir: dir.GetPath(), fName: fname, as: type)
+    }
+
+        
+    static private func writeToDisk<T: Encodable>(dir: String, fName: String, data: T) -> DatastoreError? {
+        do {
+            let url = try Datastore.createDirectoryIfNeeded(name: dir)
+            let fUrl = url.appendingPathComponent(fName)
+            
+            // Encode data into JSON format before writing
+            let encodedData = try JSONEncoder().encode(data)
+            
+            try encodedData.write(to: fUrl, options: .atomic)
+            return nil
+        } catch {
+            return DatastoreError(message: "Error saving \(fName): \(error)")
+        }
+    }
+
+    
+    private func deleteFile(dir: String, fName: String) -> DatastoreError? {
+        let fileManager = FileManager.default
+        let docDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let dataDir = docDir.appendingPathComponent(dir)
+        let filePath = dataDir.appendingPathComponent(fName).path
+        
+        do {
+            if fileManager.fileExists(atPath: filePath) {
+                try fileManager.removeItem(atPath: filePath)
+                return nil
+            } else {
+                return DatastoreError(message: "File not found at path: \(filePath)")
+            }
+        } catch {
+            return DatastoreError(message: ("Error deleting file at path \(filePath): \(error)"))
+        }
+    }
+    
+            
+    private func createDirectoryIfNeeded(name : String ) throws -> URL {
+        let fileManager = FileManager.default
+        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let dataDirectory = documentsDirectory.appendingPathComponent(name)
+
+        if !fileManager.fileExists(atPath: dataDirectory.path) {
+            try fileManager.createDirectory(at: dataDirectory, withIntermediateDirectories: true)
+        }
+
+        return dataDirectory
+    }
+    
+
+}
+
+
+
+
+func jsonEncode<T: Encodable>(_ object: T) -> Result<String> {
+    let encoder = JSONEncoder()
+    encoder.dateEncodingStrategy = .iso8601
+    do {
+        let jsonData = try encoder.encode(object)
+        if let str =  String(data: jsonData, encoding: .utf8) {
+            return Result(status: .OK, err: nil, obj: str)
+        }
+        return Result(status: .ERROR, err: DatastoreError(message: "Failed to encode object"), obj: nil)
+    } catch {
+        return Result(status: .ERROR, err: DatastoreError(message: "Failed to encode object: \(error)"), obj: nil)
+    }
+}
+
+func jsonDecode<T: Decodable>(_ jsonData: Data, as type: T.Type) -> Result<T> {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    
+
+    do {
+        let decodedObject = try decoder.decode(T.self, from: jsonData)
+        return Result(status: .OK, err: nil, obj: decodedObject)
+    } catch {
+        return Result(status: .ERROR, err: DatastoreError(message: "Failed to decode JSON: \(error)"), obj: nil)
+    }
+}
+
+
+
+func readFromDisk<T: Decodable>(dir: String, fName : String, as type: T.Type) -> Result<T> {
+    let fManager = FileManager.default
+    let docDir = fManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+    let dataDir = docDir.appendingPathComponent(dir)
+    let fUrl = dataDir.appendingPathComponent(fName)
+    do {
+        let data = try Data(contentsOf: fUrl)
+        return jsonDecode(data, as: type)
+    } catch {
+        return Result(status: .ERROR, err: DatastoreError(message: "Error reading \(fName): \(error)"), obj: nil)
+    }
+}
+
+private func writetoDisk(data : Data, dir : String, fName : String) -> DatastoreError? {
+    do {
+        let url = try createDirectoryIfNeeded(name: dir)
+        let fUrl = url.appendingPathComponent(fName)
+        let jsonData = try JSONEncoder().encode(data)
+        try jsonData.write(to: fUrl, options: .atomic)
+        return nil
+    } catch {
+        return DatastoreError(message: "Error saving \(fName): \(error)")
+    }
+}
+
+private func createDirectoryIfNeeded(name : String ) throws -> URL {
+    let fileManager = FileManager.default
+    let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+    let dataDirectory = documentsDirectory.appendingPathComponent(name)
+
+    if !fileManager.fileExists(atPath: dataDirectory.path) {
+        try fileManager.createDirectory(at: dataDirectory, withIntermediateDirectories: true)
+    }
+
+    return dataDirectory
+}
